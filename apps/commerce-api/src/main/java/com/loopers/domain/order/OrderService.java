38d4c181383adfc4e66b1473cdac699ebadacca4
Tebah(Sungkyum Kim) <sungkyum1@gmail.com>
2025-07-30 14:54:19 +0900

    [FEAT]: 주문 기능 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/order/OrderService.java
@@ -0,0 +1,20 @@
+package com.loopers.domain.order;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final OrderValidator orderValidator;
+    
+    public Order place(Long userId, List<OrderItem> items) {
+        Order order = new Order(userId, items);
+        order.place(orderValidator);
+        return orderRepository.save(order);
+    }
+}
