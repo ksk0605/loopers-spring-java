2ae2802f91dad40c1775687b9f0bbab85123469e
Tebah(Sungkyum Kim) <sungkyum1@gmail.com>
2025-07-30 14:54:26 +0900

    [FEAT]: 결제 기능 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/payment/PaymentService.java
@@ -0,0 +1,20 @@
+package com.loopers.domain.payment;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class PaymentService {
+    private final PaymentRepository paymentRepository;
+    private final PaymentValidator paymentValidator;
+
+    public Payment process(Long orderId, PaymentMethod method, BigDecimal amount) {
+        Payment payment = new Payment(orderId, method, PaymentStatus.PENDING, amount);
+        payment.process(paymentValidator);
+        return paymentRepository.save(payment);
+    }
+}
