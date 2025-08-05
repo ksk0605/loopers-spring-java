7357062743d172b2f1fe464543885e47e70ca318
Tebah(Sungkyum Kim) <sungkyum1@gmail.com>
2025-08-06 03:50:46 +0900

    [TEST]: 한명의 사용자의 동일 쿠폰 동시 사용 방지 테스트 추가

--- apps/commerce-api/src/test/java/com/loopers/domain/coupon/CouponServiceIntegrationTest.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/coupon/CouponServiceIntegrationTest.java
@@ -5,6 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.loopers.infrastructure.coupon.CouponJpaRepository;
+import com.loopers.infrastructure.coupon.UserCouponJpaRepository;
 import com.loopers.support.error.CoreException;
 import com.loopers.support.error.ErrorType;
 import com.loopers.utils.DatabaseCleanUp;
@@ -26,6 +34,9 @@ class CouponServiceIntegrationTest {
 
     @Autowired
     private CouponJpaRepository couponJpaRepository;
+
+    @Autowired
+    private UserCouponJpaRepository userCouponJpaRepository;
 
     @Autowired
     private DatabaseCleanUp databaseCleanUp;
@@ -80,6 +91,54 @@ class CouponServiceIntegrationTest {
 
             // assert
             assertThat(exception.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+        }
+
+        @DisplayName("한명의 사용자가 동일한 쿠폰을 동시에 사용하면, 쿠폰 사용에 실패한다.")
+        @Test
+        void failsToApplyCoupon_whenSameUserTriesToApplyCouponSimultaneously() throws InterruptedException {
+            // arrange
+            Coupon coupon = couponJpaRepository.save(Coupon.fixedAmount("새로운 쿠폰", "쿠폰의 설명입니다.", 5000L, 30000L, null));
+
+            ExecutorService executorService = Executors.newFixedThreadPool(2);
+            CountDownLatch countDownLatch = new CountDownLatch(2);
+
+            // act
+            List<Future<UserCoupon>> futures = new ArrayList<>();
+
+            futures.add(executorService.submit(() -> {
+                try {
+                    return couponService.apply(1L, coupon.getId(), BigDecimal.valueOf(30000));
+                } finally {
+                    countDownLatch.countDown();
+                }
+            }));
+
+            futures.add(executorService.submit(() -> {
+                try {
+                    return couponService.apply(1L, coupon.getId(), BigDecimal.valueOf(30000));
+                } finally {
+                    countDownLatch.countDown();
+                }
+            }));
+
+            countDownLatch.await();
+
+            // assert
+            long successCount = futures.stream()
+                .map(future -> {
+                    try {
+                        return future.get();
+                    } catch (Exception e) {
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .count();
+
+            assertAll(
+                () -> assertThat(successCount).isEqualTo(1),
+                () -> assertThat(userCouponJpaRepository.count()).isEqualTo(1)
+            );
         }
     }
 }
