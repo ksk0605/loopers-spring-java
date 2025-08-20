5e3f343678239eaf5f455e041a0890795ceaf2f8
Tebah(Sungkyum Kim) <sungkyum1@gmail.com>
2025-08-20 17:02:37 +0900

    [FEAT]: 결제 요청에 재시도 기능 추가

--- apps/commerce-api/src/main/java/com/loopers/infrastructure/payment/pgsimulator/PgSimulatorClient.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/payment/pgsimulator/PgSimulatorClient.java
@@ -1,12 +1,15 @@
 package com.loopers.infrastructure.payment.pgsimulator;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
+import com.loopers.infrastructure.payment.pgsimulator.exception.PgSimulatorRetryableException;
 import com.loopers.infrastructure.payment.pgsimulator.request.PgSimulatorPaymentRequest;
 import com.loopers.infrastructure.payment.pgsimulator.response.PgSimulatorApiResponse;
 import com.loopers.infrastructure.payment.pgsimulator.response.PgSimulatorTransactionDetailResponse;
@@ -15,8 +18,11 @@ import com.loopers.infrastructure.payment.pgsimulator.response.PgSimulatorTransa
 @FeignClient(name = "pg-simulator", url = "http://localhost:8082", configuration = PgSimulatorConfig.class)
 public interface PgSimulatorClient {
     @PostMapping("/api/v1/payments")
-    PgSimulatorApiResponse<PgSimulatorTransactionResponse> request(@RequestBody PgSimulatorPaymentRequest request, @RequestHeader("X-USER-ID") String userId);
+    @Retryable(value = { PgSimulatorRetryableException.class }, maxAttempts = 2, backoff = @Backoff(delay = 500))
+    PgSimulatorApiResponse<PgSimulatorTransactionResponse> request(@RequestBody PgSimulatorPaymentRequest request,
+            @RequestHeader("X-USER-ID") String userId);
 
     @GetMapping("/api/v1/payments/{transactionKey}")
-    PgSimulatorApiResponse<PgSimulatorTransactionDetailResponse> getTransaction(@PathVariable String transactionKey, @RequestHeader("X-USER-ID") String userId);
+    PgSimulatorApiResponse<PgSimulatorTransactionDetailResponse> getTransaction(@PathVariable String transactionKey,
+            @RequestHeader("X-USER-ID") String userId);
 }
