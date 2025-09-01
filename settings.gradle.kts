1c754eca6c31d13920f05e2a998ddea31a0a57aa
Tebah(Sungkyum Kim) <sungkyum1@gmail.com>
2025-09-02 01:44:01 +0900

    [FEAT]: kafka 모듈 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ include(
     ":apps:pg-simulator",
     ":modules:jpa",
     ":modules:redis",
+    ":modules:kafka",
     ":supports:jackson",
     ":supports:logging",
     ":supports:monitoring",
