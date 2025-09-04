19783475e3229ca76982ca42c7fe0d992fa0de36
Tebah(Sungkyum Kim) <sungkyum1@gmail.com>
2025-09-04 19:33:52 +0900

    feat: 비동기로 캐시 삭제 기능 구현

--- apps/commerce-streamer/src/main/kotlin/com/loopers/domain/product/ProductService.kt
+++ apps/commerce-streamer/src/main/kotlin/com/loopers/domain/product/ProductService.kt
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Component
 
 @Component
 class ProductService(
-    private val productRepository: ProductRepository
+    private val productRepository: ProductRepository,
+    private val productEventPublisher: ProductEventPublisher,
+    private val productCacheRepository: ProductCacheRepository
 ) {
     fun deduct(command: ProductCommand.Deduct) {
         val products = productRepository.findAll(command.productIds())
@@ -15,7 +17,18 @@ class ProductService(
                 val product = products.find { it.id == option.productId } ?: throw CoreException(ErrorType.NOT_FOUND)
                 product.deduct(option.productOptionId, option.quantity)
             }
-        products.filter { it.isSoldOut() }
-            .map { it.markAsSoldOut() }
+        val soldOutProducts = products.filter { it.isSoldOut() }
+        soldOutProducts.forEach { it.markAsSoldOut() }
+
+        if (soldOutProducts.isNotEmpty()) {
+            val soldOutProductIds = soldOutProducts.mapNotNull { it.id }
+            productEventPublisher.publishSoldOutEvent(soldOutProductIds)
+        }
+    }
+
+    fun deleteCaches(event: ProductSoldOutEvent) {
+        event.productIds.forEach {
+            productCacheRepository.deleteProductInfo(it)
+        }
     }
 }
