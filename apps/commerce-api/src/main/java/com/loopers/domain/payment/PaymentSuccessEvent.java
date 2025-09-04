aba64614431eb258c16fc7889d8d3696a73f28bb
Tebah(Sungkyum Kim) <sungkyum1@gmail.com>
2025-09-04 13:48:42 +0900

    feat: 결제 성공 이벤트를 카프카 이벤트로 발송

--- apps/commerce-api/src/main/java/com/loopers/domain/payment/PaymentSuccessEvent.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/payment/PaymentSuccessEvent.java
@@ -1,12 +1,44 @@
 package com.loopers.domain.payment;
 
+import java.util.Map;
+
+import com.loopers.domain.commerceevent.CommerceEventCommand;
+import com.loopers.domain.commerceevent.Publishable;
+
 import lombok.Getter;
 
 @Getter
-public class PaymentSuccessEvent {
+public class PaymentSuccessEvent extends Publishable {
+    private static final String EVENT_TYPE = "PAYMENT_SUCCESS";
     private final String orderId;
 
     public PaymentSuccessEvent(String orderId) {
+        super(EVENT_TYPE, orderId);
         this.orderId = orderId;
+    }
+
+    @Override
+    public CommerceEventCommand.Send toSendCommand() {
+        return new CommerceEventCommand.Send(
+            eventId,
+            aggregateId,
+            getPayload()
+        );
+    }
+
+    @Override
+    public CommerceEventCommand.Record toLogCommand() {
+        return new CommerceEventCommand.Record(
+            eventId,
+            EVENT_TYPE,
+            aggregateId,
+            getPayload()
+        );
+    }
+
+    private Map<String, Object> getPayload() {
+        return Map.of(
+            "orderId", orderId
+        );
     }
 }
