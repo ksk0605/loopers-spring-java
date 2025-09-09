c5be560e7bc7b434084137993d163900a746d3d8
Tebah(Sungkyum Kim) <sungkyum1@gmail.com>
2025-09-09 16:40:01 +0900

    feat: 유저 시그널 랭킹 점수 반영 기능 구현

--- apps/commerce-streamer/src/main/kotlin/com/loopers/application/rank/RankFacade.kt
+++ apps/commerce-streamer/src/main/kotlin/com/loopers/application/rank/RankFacade.kt
@@ -1,17 +1,30 @@
 package com.loopers.application.rank
 
+import com.loopers.domain.common.EventDeduplicator
 import com.loopers.domain.common.InternalMessage
 import com.loopers.domain.payment.PaymentEvent
+import com.loopers.domain.rank.RankScoreCalculator
+import com.loopers.domain.rank.RankService
 import com.loopers.domain.usersignal.UserSignalEvent
 import org.springframework.stereotype.Component
 
 @Component
-class RankFacade {
+class RankFacade(
+    private val eventDeduplicator: EventDeduplicator,
+    private val rankService: RankService,
+    private val rankScoreCalculator: RankScoreCalculator
+) {
     fun applyUserSignalToRanking(messages: List<InternalMessage<UserSignalEvent>>) {
-        // TODO
+        val userSignalEvents = messages
+            .filter { eventDeduplicator.checkAndMarkAsProcessed(it.metadata.eventId) }
+            .map { it.payload }
+        val scoreMap = rankScoreCalculator.calculateFromUserSignals(userSignalEvents)
+        if (scoreMap.isNotEmpty()) {
+            rankService.applyScores(scoreMap)
+        }
     }
 
     fun applyOrderToRanking(messages: List<InternalMessage<PaymentEvent>>) {
-        // TODO
+        // TODO: 주문 점수 계산
     }
 }
